e1431781e2092400a69f1732003f8c17e4ded057
diego <beltrd@gmail.com>
2020-05-14 19:47:11 -0500

    created function that accepts username and logins to ig using env vars

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const cheerios = require('cheerio');
 const getUrls = require('get-urls');
 const fetch = require('node-fetch');
 const puppeteer = require('puppeteer');
+
+const scrapeImages = async (username) => {
+    const browser = await puppeteer.launch({ headless : false });
+    const page = await browser.newPage();
+    await page.goto('https://www.instagram.com/accounts/login/');
+    await page.setViewport({ width: 1000, height: 500 });
+    await page.waitFor(200);
+    await page.type('[name="username"]', process.env.INSTA_USERNAME);
+    await page.type('[name="password"]', process.env.INSTA_PASSWORD);
+    await page.click('[type=submit]');
+}
